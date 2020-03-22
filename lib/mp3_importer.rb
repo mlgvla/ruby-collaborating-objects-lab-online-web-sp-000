00d1cc5c57dc14e5ff5de7ea91bb00eecb210345
Monica Gerard <mlgerardvla@gmail.com>
2020-03-22 23:08:10 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,17 @@
 require 'pry'
 class MP3Importer
-  attr_accessor :path, :mp3_files
+  attr_accessor :path
 
 
   def initialize(path)
     @path = path
-    @mp3_files = []
   end
 
   def files
     files = []
     files = Dir.new(path).entries
-    @mp3_files = files.select {|file| file.include?(".mp3")}
-    @mp3_files
+    mp3_files = files.select {|file| file.include?(".mp3")}
+    mp3_files
   end
 
   def import
